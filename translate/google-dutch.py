4c3d3b4d012e87ea785e06afef61d70a9998d7dd
marieke.weultjes <mariekeweultjes@hotmail.com>
2022-04-07 15:53:14 +0200

    made changes to test smaller amount of data and write to a new file

--- translate/google-dutch.py
+++ translate/google-dutch.py
@@ -13,6 +13,8 @@ def get_lines_from_file(filename):
             line = line.strip()
             english_sentences.append(line)
 
+    f.close()
+
     return english_sentences
 
 
@@ -27,28 +29,34 @@ def translate(sentences):
     for line in sentences:
         dutch_translation = translator.translate(line, dest="nl")
         translated_sentences.append(dutch_translation)
-        print(f"{dutch_translation.origin} ({dutch_translation.src}) --> {dutch_translation.text} ({dutch_translation.dest})")
+        print("translation done")
 
     return translated_sentences
 
-#def create_dutch_file(filename):
-#    """create a new file to use for dutch style transfer"""
+def create_new_data_file(translated_sentences, new_filename):
+    """create a new file to use for dutch style transfer"""
+    with open(new_filename, 'w') as f:
+        for line in translated_sentences:
+            f.write(line)
+
+    f.close()
 
 def main():
 
     # gather all English data; for macbook use path "../data/em/train.0"
-    train_src_en = get_lines_from_file("/data/s3238903/style_transfer/data/em/train.0")
+    #train_src_en = get_lines_from_file("/data/s3238903/style_transfer/data/em/train.0")
     #train_tgt_en = get_lines_from_file("/data/s3238903/style_transfer/data/em/train.1")
+    valid_src_en = get_lines_from_file("./data/em/valid.0")
     #valid_src_en = get_lines_from_file("/data/s3238903/style_transfer/data/em/valid.0")
     #valid_tgt_en = get_lines_from_file("/data/s3238903/style_transfer/data/em/valid.1")
 
     # translate all English sentences
-    train_src_nl = translate(train_src_en)
+    #train_src_nl = translate(train_src_en)
     #train_tgt_nl = translate(train_tgt_en)
-    #valid_src_nl = translate(valid_src_en)
+    valid_src_nl = translate(valid_src_en)
     #valid_tgt_nl = translate(valid_tgt_en)
 
-    print(train_src_nl)
+    print(valid_src_nl)
 
 
 if __name__ == '__main__':
