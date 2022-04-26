10b94a42fd888b1bc65a553c9ff8a7202fcf54b5
marieke.weultjes <mariekeweultjes@hotmail.com>
2022-04-26 11:41:31 +0200

    test completed, added all data

--- translate/google-translate.py
+++ translate/google-translate.py
@@ -58,24 +58,24 @@ def main():
 
     # gather all English data; for macbook use path "../data/em/train.0"
     print("Gathering Data...")
-    #train_src_en = get_lines_from_file("/data/s3238903/dutch-style-transfer/data/em/train.0")
-    #train_tgt_en = get_lines_from_file("/data/s3238903/dutch-style-transfer/data/em/train.1")
+    train_src_en = get_lines_from_file("/data/s3238903/dutch-style-transfer/data/em/train.0")
+    train_tgt_en = get_lines_from_file("/data/s3238903/dutch-style-transfer/data/em/train.1")
     valid_src_en = get_lines_from_file("/data/s3238903/dutch-style-transfer/data/em/valid.0")
-    #valid_tgt_en = get_lines_from_file("/data/s3238903/dutch-style-transfer/data/em/valid.1")
+    valid_tgt_en = get_lines_from_file("/data/s3238903/dutch-style-transfer/data/em/valid.1")
 
     # translate all English sentences
     print("Start Translation...")
-    #train_src_nl = translate(train_src_en,"1")
-    #train_tgt_nl = translate(train_tgt_en,"2")
+    train_src_nl = translate(train_src_en,"1")
+    train_tgt_nl = translate(train_tgt_en,"2")
     valid_src_nl = translate(valid_src_en,"3")
-    #valid_tgt_nl = translate(valid_tgt_en,"4")
+    valid_tgt_nl = translate(valid_tgt_en,"4")
 
     print("Writing to new file...")
     #create_new_data_file(valid_src_nl,"./data/em-dutch/valid.0")
-    #create_new_data_file(train_src_nl,"/data/s3238903/dutch-style-transfer/data/em-dutch/train.0","1")
-    #create_new_data_file(train_tgt_nl,"/data/s3238903/dutch-style-transfer/data/em-dutch/train.1","2")
+    create_new_data_file(train_src_nl,"/data/s3238903/dutch-style-transfer/data/em-dutch/train.0","1")
+    create_new_data_file(train_tgt_nl,"/data/s3238903/dutch-style-transfer/data/em-dutch/train.1","2")
     create_new_data_file(valid_src_nl,"/data/s3238903/dutch-style-transfer/data/em-dutch/valid.0","3")
-    #create_new_data_file(valid_tgt_nl,"/data/s3238903/dutch-style-transfer/data/em-dutch/valid.1","4")
+    create_new_data_file(valid_tgt_nl,"/data/s3238903/dutch-style-transfer/data/em-dutch/valid.1","4")
 
     print("Script Completed")
 
