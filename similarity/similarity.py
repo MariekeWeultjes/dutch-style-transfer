a475ba7046f34d86648c5e78817b7621206376db
Marieke Weultjes <mariekeweultjes@hotmail.com>
2023-03-21 09:10:39 +0100

    few changes similarity

--- similarity/similarity.py
+++ similarity/similarity.py
@@ -37,7 +37,7 @@ def main():
 	#formal_a2 = []
 
 	# create lists to store scores
-	inf_a0_scores, inf_a1_scores, inf_a2_scores, a0_a1_scores, a0_a2_scores, a1_a2, inf_for = [], [], [], [], [], [], []
+	inf_a0_scores, inf_a1_scores, inf_a2_scores, a0_a1_scores, a0_a2_scores, a1_a2_scores, inf_for_scores = [], [], [], [], [], [], []
 
 	# create list with the instances and their scores to get some more detailed insights
 	all_scores, all_scores_all_references = [], []
@@ -51,11 +51,6 @@ def main():
 		t_a1 = word_tokenize(formal_a1_sen[i], language='dutch')
 		t_a2 = word_tokenize(formal_a2_sen[i], language='dutch')
 		t_all_references = [t_a0, t_a1, t_a2]
-		# add tokenized sentence to list for corpus bleu, MIGHT BE REMOVED
-		#informal.append(t_inf)
-		#formal_a0.append(t_a0)
-		#formal_a1.append(t_a1)
-		#formal_a2.append(t_a2)
 		# calculcate sentence blue scores
 		inf_a0 = round(sentence_bleu([t_inf], t_a0, smoothing_function=smooth.method1), 4)
 		inf_a1 = round(sentence_bleu([t_inf], t_a1, smoothing_function=smooth.method1), 4)
@@ -81,22 +76,19 @@ def main():
 		all_scores.append([t_a1, t_a2, a1_a2])
 		all_scores_all_references.append([t_a0, t_a1, t_a2, t_inf, inf_for])
 		# calculate comet scores
-        inputs = [{"src": "", "mt": t_a0[i], "ref": informal_sen[i]}]
+        inputs = [{"src": "", "mt": formal_a0_sen[i], "ref": informal_sen[i]}]
         comet_score = comet_model.predict(inputs, batch_size=8, gpus=1)
-        inputs = [{"src": "", "mt": t_a1[i], "ref": informal_sen[i]}]
+        # TO DO?: Append comet score to list
+        inputs = [{"src": "", "mt": formal_a1_sen[i], "ref": informal_sen[i]}]
         comet_score = comet_model.predict(inputs, batch_size=8, gpus=1)
-        inputs = [{"src": "", "mt": t_a2[i], "ref": informal_sen[i]}]
+        inputs = [{"src": "", "mt": formal_a2_sen[i], "ref": informal_sen[i]}]
         comet_score = comet_model.predict(inputs, batch_size=8, gpus=1)
-
-	print("Starting Corpus Bleu Scores...")
-
-	# calculate corpus bleu scores, MIGHT BE REMOVED
-	#print("corpus BLEU score inf-a0 = ", round(corpus_bleu(informal, formal_a0), 4))
-	#print("corpus BLEU score inf-a1 = ", round(corpus_bleu(informal, formal_a1), 4))
-	#print("corpus BLEU score inf-a2 = ", round(corpus_bleu(informal, formal_a2), 4))
-	#print("corpus BLEU score a0-a1 = ", round(corpus_bleu(formal_a0, formal_a1), 4))
-	#print("corpus BLEU score a0-a2 = ", round(corpus_bleu(formal_a0, formal_a2), 4))
-	#print("corpus BLEU score a1-a2 = ", round(corpus_bleu(formal_a1, formal_a2), 4))
+        inputs = [{"src": "", "mt": formal_a1_sen[i], "ref": formal_a0_sen[i]}]
+        comet_score = comet_model.predict(inputs, batch_size=8, gpus=1)
+        inputs = [{"src": "", "mt": formal_a2_sen[i], "ref": formal_a0_sen[i]}]
+        comet_score = comet_model.predict(inputs, batch_size=8, gpus=1)
+        inputs = [{"src": "", "mt": formal_a2_sen[i], "ref": formal_a1_sen[i]}]
+        comet_score = comet_model.predict(inputs, batch_size=8, gpus=1)
 
 	# calculate average BLEU of individual sentence scores
 	print("sentence BLEU score inf-a0 = ", np.mean(inf_a0_scores))
@@ -106,8 +98,6 @@ def main():
 	print("sentence BLEU score a0-a2 = ", np.mean(a0_a2_scores))
 	print("sentence BLEU score a1-a2 = ", np.mean(a1_a2_scores))
 	print("sentence BLEU score inf_for = ", np.mean(inf_for_scores))
-
-
 
 	# put sentences and their sentence_bleu score in a file for futher investigation
 	with open('similarty_scores.csv', 'w', newline='') as f:
